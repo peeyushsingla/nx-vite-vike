152923607c3aa26dbef3bfb0751d8cc6e36af8a7
Peeyush Singla <peeyush.singla@metycle.com>
2024-02-02 13:17:18 +0100

    first commit

---
+++ my-app/vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import vike from 'vike/plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../node_modules/.vite/my-app',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [react(), vike({
+    prerender: true,
+  }),  nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../dist/my-app',
+  },
+
+});
